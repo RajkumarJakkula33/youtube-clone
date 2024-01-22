c6d4c57855cb5d9b6ee93d7e104c8437eaef9ec3
Rajkumar Jakkula <rajkumarjakkula@Rajkumars-MacBook-Air.local>
2024-01-21 20:06:26 -0500

    setting acl to private for s3

--- statefiles3-TF/main.tf
+++ statefiles3-TF/main.tf
@@ -1,10 +1,21 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "eks-youtube-statefile-837"
-  acl    = "private"     
-  lifecycle {
-    prevent_destroy = true
+  bucket = "eks-youtube-statefile-837"  
+  # lifecycle {
+  #   prevent_destroy = true  (prevent_destroy = true. This means that if someone tries to run terraform destroy, Terraform will prevent the destruction of s3bucket)
+}
+
+resource "aws_s3_bucket_ownership_controls" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
   }
 }
+
+resource "aws_s3_bucket_acl" "terraform_state_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.terraform_state]
+  bucket = aws_s3_bucket.terraform_state.id
+  acl = "private"  # or use "public-read" or "public-read-write" based on your requirements
+  }
 
 resource "aws_s3_bucket_versioning" "terraform_state" {
     bucket = aws_s3_bucket.terraform_state.id
