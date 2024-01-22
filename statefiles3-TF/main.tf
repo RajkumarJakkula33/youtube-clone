13baddbee20a19e69924f629a75bed1817693005
Rajkumar Jakkula <149739168+RajkumarJakkula33@users.noreply.github.com>
2024-01-21 19:45:31 -0500

    Update main.tf

--- statefiles3-TF/main.tf
+++ statefiles3-TF/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "eks-youtube-statefile-837"
-     
+  acl    = "private"     
   lifecycle {
     prevent_destroy = true
   }
@@ -11,4 +11,13 @@ resource "aws_s3_bucket_versioning" "terraform_state" {
     versioning_configuration {
       status = "Enabled"
     }
+}
+resource "aws_dynamodb_table" "lock_table" {
+  name           = "TFLOCK"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
