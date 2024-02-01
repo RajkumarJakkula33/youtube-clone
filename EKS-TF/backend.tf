179fd08401f7041b31569e5c1340a0912f26e1f3
Rajkumar Jakkula <rajkumarjakkula@Rajkumars-MacBook-Air.local>
2024-02-01 15:11:26 -0500

    update

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "eks-youtube-statefile-837"
-#     key            = "backend/EKS.tfstate"
-#     region         = "us-east-1"
-#     encrypt        = true
-#     dynamodb_table = "your-lock-table"
-# }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "eks-youtube-statefile-837"
+    key            = "backend/EKS.tfstate"
+    region         = "us-east-1"
+    # encrypt        = true
+    # dynamodb_table = "your-lock-table"
+}
+}
