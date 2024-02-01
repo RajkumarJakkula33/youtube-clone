d52c2ac74b63cbd6745743e3eddd9c24c8f3d913
Rajkumar Jakkula <149739168+RajkumarJakkula33@users.noreply.github.com>
2024-02-01 14:55:34 -0500

    Update main.tf

--- statefiles3-TF/main.tf
+++ statefiles3-TF/main.tf
@@ -23,12 +23,3 @@ resource "aws_s3_bucket_versioning" "terraform_state" {
       status = "Enabled"
     }
 }
-resource "aws_dynamodb_table" "lock_table" {
-  name           = "TFLOCK"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
